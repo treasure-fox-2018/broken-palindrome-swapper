aa144cec275de1e116a9debd54492e0c5764beac
Fadli Al Baihaqi <alibaihaqi1704@gmail.com>
2018-05-29 20:56:49 +0700

    tugas broken-palindrome Fadli Al Baihaqi beres

--- palindromeSwapper.js
+++ palindromeSwapper.js
@@ -8,14 +8,15 @@
 function palindromeSwapper(str) {
   let newStr = '';
   for (var i = 0; i < str.length; i++) {
-    for (var j = 0; j < str.length; i++) {
+    for (var j = 0; j < str.length; j++) { //penulisan i++ diganti menjadi j++
       if (i === j) {
-        newStr += str[j++];
+        newStr += str[j+1];//yang dilakukan perubahan adalah str[j++] menjadi str[j+1] karena yang diinginkan adalah penukaran pada karakter setelahnya
         newStr += str[j];
         j++;
       } else {
         newStr += str[j];
       }
+      debugger
     }
     if (isPalindrome(newStr)) return true;
   }
@@ -29,8 +30,9 @@ function palindromeSwapper(str) {
  * @returns {boolean} true bila kata adalah palindrom
  */
 function isPalindrome(str) {
-  if (str.split('').reverse().join() === str) return console.log(true);
-  return console.log(false);
+  if (str.split('').reverse().join('') === str) return true; //dilakukan pengecekan penulisan join() menjadi join('')
+  debugger                                                   //dan ketika dilakukan 'return' tidak perlu menggunakan console.log lagi sehingga diganti langsung return true
+  return false;//sama seperti yang return true, dihapus return console.log menjadi return false
 }
 
 console.log(palindromeSwapper('arcecar')); // TRUE
