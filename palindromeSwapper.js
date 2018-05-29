f9fa78dc80f9764c723011f5cdee55c91bb7cfdb
rizariza69 <riza.riza69@gmail.com>
2018-05-29 23:52:31 +0700

    palindrome done

--- palindromeSwapper.js
+++ palindromeSwapper.js
@@ -8,16 +8,18 @@
 function palindromeSwapper(str) {
   let newStr = '';
   for (var i = 0; i < str.length; i++) {
-    for (var j = 0; j < str.length; i++) {
+    for (var j = 0; j < str.length; j++) { // buka i++ tapi j++
       if (i === j) {
-        newStr += str[j++];
+        newStr += str[j+1]; // Plus 1, buka ++
         newStr += str[j];
         j++;
       } else {
         newStr += str[j];
       }
     }
-    if (isPalindrome(newStr)) return true;
+    if (isPalindrome(newStr)) { // kurang {} untuk return true
+        return true
+    };
   }
   return false;
 }
@@ -29,8 +31,13 @@ function palindromeSwapper(str) {
  * @returns {boolean} true bila kata adalah palindrom
  */
 function isPalindrome(str) {
-  if (str.split('').reverse().join() === str) return console.log(true);
-  return console.log(false);
+
+  if (str.split('').reverse().join('') === str) { // join dida;am sintax kurang ('')
+    return true  // kurang penggunaan {} dan return true dan return false bukan menggunaan console.log()
+  } else {
+    return false
+  }
+
 }
 
 console.log(palindromeSwapper('arcecar')); // TRUE
