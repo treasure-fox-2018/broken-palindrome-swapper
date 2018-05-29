c2ab29388b6ea66dd371eec76ce38ab532b2e91a
vinesto <oqvinesto@gmail.com>
2018-05-29 14:39:46 +0700

    first commit

--- palindromeSwapper.js
+++ palindromeSwapper.js
@@ -8,15 +8,21 @@
 function palindromeSwapper(str) {
   let newStr = '';
   for (var i = 0; i < str.length; i++) {
-    for (var j = 0; j < str.length; i++) {
+    for (var j = 0; j < str.length; j++) {
       if (i === j) {
-        newStr += str[j++];
-        newStr += str[j];
+        // console.log(str[j+1])
+        newStr += str[j+1]; // before newStr += str[j++]
+        newStr += str[i]; // before newStr += str[j]
+        // console.log(newStr)
         j++;
+        // console.log(j)
       } else {
         newStr += str[j];
       }
+      // debugger;
     }
+    // debugger;
+    // console.log(isPalindrome(newStr))
     if (isPalindrome(newStr)) return true;
   }
   return false;
@@ -29,8 +35,8 @@ function palindromeSwapper(str) {
  * @returns {boolean} true bila kata adalah palindrom
  */
 function isPalindrome(str) {
-  if (str.split('').reverse().join() === str) return console.log(true);
-  return console.log(false);
+  if (str.split('').reverse().join("") === str) return true; // before join(), before console.log(return true)
+  return false; // before console.log(return false)
 }
 
 console.log(palindromeSwapper('arcecar')); // TRUE
