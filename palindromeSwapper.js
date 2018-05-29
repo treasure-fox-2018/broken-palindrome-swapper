3029b0984075b0bf97d15cbe1c1bb4a4896f6c2b
susiantiamelia <ameliasusianti@gmail.com>
2018-05-29 09:16:18 +0700

    Debugging done

--- palindromeSwapper.js
+++ palindromeSwapper.js
@@ -6,18 +6,20 @@
  * @returns {boolean} true bila kata yang di-swap adalah palindrom
  */
 function palindromeSwapper(str) {
-  let newStr = '';
   for (var i = 0; i < str.length; i++) {
-    for (var j = 0; j < str.length; i++) {
-      if (i === j) {
-        newStr += str[j++];
-        newStr += str[j];
-        j++;
-      } else {
+    let newStr = '';
+    for (var j = 0; j < str.length; j++) {
+      if (i === j && i !== str.length-1) {
+        newStr += str[j+1];
+        newStr += str[j]; 
+        
+       
+      } else if(i+1 !== j || i === str.length-1){
         newStr += str[j];
       }
     }
-    if (isPalindrome(newStr)) return true;
+    
+    if (isPalindrome(newStr)){ return true;}
   }
   return false;
 }
@@ -29,8 +31,13 @@ function palindromeSwapper(str) {
  * @returns {boolean} true bila kata adalah palindrom
  */
 function isPalindrome(str) {
-  if (str.split('').reverse().join() === str) return console.log(true);
-  return console.log(false);
+  var reverse = str.split('').reverse()
+  var cek = ''
+  for(var i = 0; i < reverse.length; i++){
+    cek += reverse[i]
+  }
+  if (cek === str ){return true;}
+  return false;
 }
 
 console.log(palindromeSwapper('arcecar')); // TRUE
