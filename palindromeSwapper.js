aea689d293b3483b2d80de49534ce629c3c4f318
Setia Anggraeni <tia.anggraeni2@gmail.com>
2018-05-29 15:59:08 +0700

    Palindrome Swapper

--- palindromeSwapper.js
+++ palindromeSwapper.js
@@ -6,17 +6,18 @@
  * @returns {boolean} true bila kata yang di-swap adalah palindrom
  */
 function palindromeSwapper(str) {
-  let newStr = '';
-  for (var i = 0; i < str.length; i++) {
-    for (var j = 0; j < str.length; i++) {
+  for (var i = 0; i <= str.length; i++) {
+    let newStr = ''; // move newStr over here, biar setelah looping j newStr kosong lagi
+    for (var j = 0; j < str.length; j++) {
       if (i === j) {
-        newStr += str[j++];
+        newStr += str[j+1];
         newStr += str[j];
         j++;
       } else {
         newStr += str[j];
-      }
+        }
     }
+    // console.log(newStr)
     if (isPalindrome(newStr)) return true;
   }
   return false;
@@ -29,8 +30,8 @@ function palindromeSwapper(str) {
  * @returns {boolean} true bila kata adalah palindrom
  */
 function isPalindrome(str) {
-  if (str.split('').reverse().join() === str) return console.log(true);
-  return console.log(false);
+  if (str.split('').reverse().join("") === str) return true;
+  return false; // klu udh return jgn campur sm console.log() ntr jd double
 }
 
 console.log(palindromeSwapper('arcecar')); // TRUE
