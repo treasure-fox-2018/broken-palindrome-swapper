a62f3b911ec862d07825f07fdbd066bafd9b3db6
Yusuf Salman Noerinaldy Siregar <yusufsiregar5@gmail.com>
2018-05-29 12:43:11 +0700

    finish first challenge

--- palindromeSwapper.js
+++ palindromeSwapper.js
@@ -6,18 +6,27 @@
  * @returns {boolean} true bila kata yang di-swap adalah palindrom
  */
 function palindromeSwapper(str) {
-  let newStr = '';
-  for (var i = 0; i < str.length; i++) {
-    for (var j = 0; j < str.length; i++) {
-      if (i === j) {
-        newStr += str[j++];
-        newStr += str[j];
-        j++;
-      } else {
-        newStr += str[j];
+  debugger;
+  if (isPalindrome(str)) {
+    debugger;
+    return true
+  } else {
+
+    for (var i = 0; i < str.length; i++) {
+      let newStr = '';
+      for (var j = 0; j < str.length; i++) {
+        var positionNext = j + 1;
+        if (i === j) {
+          newStr += str[positionNext];
+          newStr += str[j];
+          j += 2;
+        } else {
+          newStr += str[j];
+          j++;
+        }
       }
+      if (isPalindrome(newStr)) return true;
     }
-    if (isPalindrome(newStr)) return true;
   }
   return false;
 }
@@ -29,10 +38,14 @@ function palindromeSwapper(str) {
  * @returns {boolean} true bila kata adalah palindrom
  */
 function isPalindrome(str) {
-  if (str.split('').reverse().join() === str) return console.log(true);
-  return console.log(false);
+  var compare = str.split('').reverse().join("");
+  if (compare === str) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
-console.log(palindromeSwapper('arcecar')); // TRUE
-console.log(palindromeSwapper('racecar')); // TRUE
+// console.log(palindromeSwapper('arcecar')); // TRUE
+// console.log(palindromeSwapper('racecar')); // TRUE
 console.log(palindromeSwapper('recacar')); // FALSE
